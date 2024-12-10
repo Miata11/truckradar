0b21ea62b63025263cc2a9af23d94805763d522d
Claire Cagnat <cagnatclaire@gmail.com>
2024-12-10 15:24:12 +0100

    search valide

--- app/controllers/foodtrucks_controller.rb
+++ app/controllers/foodtrucks_controller.rb
@@ -1,2 +1,10 @@
 class FoodtrucksController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[index]
+  def index
+    if params[:query].present?
+      @foodtrucks = PgSearch.multisearch(params[:query])
+    else
+      @foodtrucks = Foodtruck.all
+    end
+  end
 end
