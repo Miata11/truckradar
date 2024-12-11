a71357718a49da6075ff1db63e22158653688595
Charline Menant <mcharline407@hotmail.com>
2024-12-11 15:54:08 +0100

    localisation en temps réel simple sans conditions

--- app/controllers/foodtrucks_controller.rb
+++ app/controllers/foodtrucks_controller.rb
@@ -1,5 +1,5 @@
 class FoodtrucksController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[index]
+  skip_before_action :authenticate_user!, only: %i[index update_guest_location]
 
   def index
     if params[:query].present?
@@ -17,18 +17,46 @@ class FoodtrucksController < ApplicationController
       }
     end
 
-    # Marker pour l'utilisateur
-    if current_user&.latitude.present?
-      @markers << {
-        lat: current_user.latitude,
-        lng: current_user.longitude,
-        info_window_html: render_to_string(partial: "user_popup", locals: { user: current_user }),
-        marker_html: render_to_string(partial: "user_marker")
-      }
+    # Ajout du marker pour l'utilisateur connecté ou invité
+    if user_signed_in? && current_user.latitude.present?
+      @markers << user_marker(current_user)
+    elsif session[:guest_latitude].present?
+      @markers << guest_marker
     end
   end
 
-  def show
+  def update_guest_location
+    session[:guest_latitude] = params[:latitude]
+    session[:guest_longitude] = params[:longitude]
+    render json: { success: true }
+  end
+
+  def update_location
     @foodtruck = Foodtruck.find(params[:id])
+    if @foodtruck.update(latitude: params[:latitude], longitude: params[:longitude])
+      render json: { success: true }
+    else
+      render json: { success: false }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_marker(user)
+    {
+      lat: user.latitude,
+      lng: user.longitude,
+      info_window_html: render_to_string(partial: "user_popup", locals: { user: user }),
+      marker_html: render_to_string(partial: "user_marker")
+    }
+  end
+
+  def guest_marker
+    {
+      lat: session[:guest_latitude],
+      lng: session[:guest_longitude],
+      info_window_html: render_to_string(partial: "guest_popup"),
+      marker_html: render_to_string(partial: "guest_marker")
+    }
   end
 end
