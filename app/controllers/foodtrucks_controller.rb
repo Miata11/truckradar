cd5f168ac4a719f6c64662e47159352d0c277c7b
Charline Menant <mcharline407@hotmail.com>
2024-12-11 12:23:19 +0100

    localisation avec address-default

--- app/controllers/foodtrucks_controller.rb
+++ app/controllers/foodtrucks_controller.rb
@@ -3,29 +3,32 @@ class FoodtrucksController < ApplicationController
 
   def index
     if params[:query].present?
-      # @foodtrucks = PgSearch.multisearch(params[:query])
       @foodtrucks = Foodtruck.search_by_foodtrucks(params[:query])
-      # The `geocoded` scope filters only flats with coordinates
-      @markers = @foodtrucks.geocoded.map do |foodtruck|
-      {
-        lat: foodtruck.latitude,
-        lng: foodtruck.longitude,
-        info_window_html: render_to_string(partial:"popupmap", locals: {foodtruck: foodtruck})
-      }
-      end
     else
-      @markers = Foodtruck.all.geocoded.map do |foodtruck|
+      @foodtrucks = Foodtruck.all
+    end
+
+    @markers = @foodtrucks.geocoded.map do |foodtruck|
       {
         lat: foodtruck.latitude,
         lng: foodtruck.longitude,
-        info_window_html: render_to_string(partial:"popupmap", locals: {foodtruck: foodtruck})
+        info_window_html: render_to_string(partial: "popupmap", locals: { foodtruck: foodtruck }),
+        marker_html: render_to_string(partial: "foodtruck_marker")
       }
-      end
+    end
+
+    # Marker pour l'utilisateur
+    if current_user&.latitude.present?
+      @markers << {
+        lat: current_user.latitude,
+        lng: current_user.longitude,
+        info_window_html: render_to_string(partial: "user_popup", locals: { user: current_user }),
+        marker_html: render_to_string(partial: "user_marker")
+      }
     end
   end
 
   def show
     @foodtruck = Foodtruck.find(params[:id])
   end
-
 end
