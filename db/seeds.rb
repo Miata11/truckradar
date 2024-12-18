bf40e2e6fea10e628db2977ad54964af92148386
Charline Menant <mcharline407@hotmail.com>
2024-12-18 14:02:59 +0100

    MAJ seeds France

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,106 @@
 require "open-uri"
-# On configure Geocoder pour ne rien faire pendant le seed
+# Configure Geocoder to do nothing during seed
 Geocoder.configure(lookup: :null)
 
+# Coordonnées de Le Teich
+LE_TEICH_LAT = 44.6375
+LE_TEICH_LON = -1.0246
+
+# Méthode pour générer des coordonnées autour de Le Teich
+def generate_coordinates_around_le_teich
+  # Approximativement 0.1 degré = ~11km
+  radius = 0.09  # légèrement inférieur pour rester dans un rayon de 10km
+
+  # Génération aléatoire dans un cercle
+  r = radius * Math.sqrt(rand)
+  theta = rand * 2 * Math::PI
+
+  lat = LE_TEICH_LAT + r * Math.cos(theta)
+  lon = LE_TEICH_LON + r * Math.sin(theta)
+
+  [lat, lon]
+end
+
+# Arrays for random selection
+CITIES = [
+  ["Paris", 48.8566, 2.3522],
+  ["Lyon", 45.7640, 4.8357],
+  ["Marseille", 43.2962, 5.3700],
+  ["Toulouse", 43.6047, 1.4442],
+  ["Nice", 43.7102, 7.2620],
+  ["Nantes", 47.2184, -1.5536],
+  ["Montpellier", 43.6108, 3.8767],
+  ["Strasbourg", 48.5734, 7.7521],
+  ["Bordeaux", 44.8378, -0.5792],
+  ["Lille", 50.6292, 3.0573],
+  ["Rennes", 48.1173, -1.6778],
+  ["Reims", 49.2628, 4.0347],
+  ["Le Mans", 47.9956, 0.1907],
+  ["Saint-Étienne", 45.4397, 4.3879],
+  ["Toulon", 43.1242, 5.9280],
+  ["Grenoble", 45.1885, 5.7245],
+  ["Dijon", 47.3223, 5.0415],
+  ["Angers", 47.4739, -0.5515],
+  ["Brest", 48.4066, -4.4960],
+  ["Le Havre", 49.4944, 0.1076],
+  ["Orléans", 47.9034, 1.9062],
+  ["Bourges", 47.0840, 2.3937],
+  ["Blois", 47.5846, 1.3272],
+  ["Châteauroux", 46.8146, 1.6953],
+  ["Tours", 47.3942, 0.6896]
+]
+
+FOOD_CATEGORIES = ["fr", "asiatique", "mexicain", "végétarien", "burger", "italien", "cuisine de saison", "barbecue", "poisson", "vegan"]
+FOOD_NAMES = [
+  "Le Truck Gourmand",
+  "Street Food Nation",
+  "Roulant Délice",
+  "Cuisine Mobile",
+  "Flavors on Wheels",
+  "Le Camion Gourmet",
+  "Urban Bites",
+  "Foodie Express",
+  "Saveurs Nomades",
+  "Le Chef Vagabond"
+]
+
+DISH_TITLES = [
+  "Plat Signature",
+  "Classic Burger",
+  "Tacos Spéciaux",
+  "Salade Fraîcheur",
+  "Burger du Jour",
+  "Pizza Artisanale",
+  "Veggie Bowl",
+  "Sandwich Gourmet",
+  "Plat du Chef",
+  "Street Food Royale"
+]
+
+STOCK_PHOTOS = [
+  "https://otuktuk.fr/wp-content/uploads/2017/10/o_tuk_tuk_fodd_truck_toulouse_slider.jpg",
+  "https://les-seminaires.eu/wp-content/uploads/2023/07/food-truck-entreprise.jpg",
+  "https://static.wixstatic.com/media/b45c09_d3b790add4cc4097afd338d22fe0f774~mv2.jpg/v1/fill/w_510,h_638,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/IMG_20240911_101704.jpg",
+  "https://static.wixstatic.com/media/369974_4b44793445374aee8e776c139cb8eedb~mv2.jpg/v1/crop/x_0,y_27,w_960,h_585/fill/w_434,h_270,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/369974_4b44793445374aee8e776c139cb8eedb~mv2.jpg"
+]
+
+DISH_PHOTOS = [
+  "https://cdn.shopify.com/s/files/1/0605/9720/7189/files/20240821143137-img_1429.jpg?v=1724250702&width=1600&height=900",
+  "https://www.cuisineactuelle.fr/imgre/fit/~1~cac~2023~01~17~999fccbd-d5ad-4cc2-91ba-d5a06206b882.jpeg/650x325/quality/80/crop-from/center/galette-de-sarrasin-a-l-ancienne.jpg",
+  "https://www.ma-grande-taille.com/wp-content/uploads/2020/01/Une-idee-recettes-salees-sucrees-vegan-chandeleur-2020.jpg"
+]
+
+FIRST_NAMES = ["Sophie", "Emma", "Léa", "Julie", "Thomas", "Antoine", "Lucas", "Pierre", "Alexandre", "Nicolas"]
+LAST_NAMES = ["Dubois", "Martin", "Bernard", "Petit", "Laurent", "Garcia", "Rodriguez", "Moreau", "Simon", "Lefebvre"]
+
+# Destroy existing records to start fresh
 Dish.destroy_all
 Favorite.destroy_all
 Foodtruck.destroy_all
 User.destroy_all
 
-
-user1 =  User.create!(
+# Existing profiles to preserve
+user1 = User.create!(
   first_name: "Claire",
   last_name: "Cagnat",
   email: "claire@gmail.com",
@@ -18,7 +110,7 @@ user1 =  User.create!(
   longitude: -0.580036,
   role: "false")
 
-user1 =  User.create!(
+user2 = User.create!(
   first_name: "Soukaina",
   last_name: "El Ataoui",
   email: "soukaina@gmail.com",
@@ -28,7 +120,7 @@ user1 =  User.create!(
   longitude: -0.580036,
   role: "false")
 
-user_pro1 =  User.create!(
+user_pro1 = User.create!(
   first_name: "Max",
   last_name: "El Poirier",
   email: "max@gmail.com",
@@ -38,7 +130,7 @@ user_pro1 =  User.create!(
   longitude: -0.580036,
   role: "true")
 
-user_pro2 =  User.create!(
+user_pro2 = User.create!(
   first_name: "Charline",
   last_name: "Menant",
   email: "charline@gmail.com",
@@ -48,95 +140,7 @@ user_pro2 =  User.create!(
   longitude: -0.580036,
   role: "true")
 
-user_pro3 = User.create!(
- first_name: "Sophie",
- last_name: "Dubois",
- email: "sophie.dubois@gmail.com",
- password: "123456",
- address_default: "45 rue Saint-Rémi, Bordeaux",
- latitude: 44.841657,
- longitude: -0.571952,
- role: "true"
-)
-
-user_pro4 = User.create!(
- first_name: "Thomas",
- last_name: "Martin",
- email: "thomas.martin@gmail.com",
- password: "123456",
- address_default: "25 rue du Palais Gallien, Bordeaux",
- latitude: 44.848234,
- longitude: -0.578901,
- role: "true"
-)
-
-user_pro5 = User.create!(
- first_name: "Julie",
- last_name: "Petit",
- email: "julie.petit@gmail.com",
- password: "123456",
- address_default: "156 cours Victor Hugo, Bordeaux",
- latitude: 44.832145,
- longitude: -0.569872,
- role: "true"
-)
-
-user_pro6 = User.create!(
- first_name: "Lucas",
- last_name: "Bernard",
- email: "lucas.bernard@gmail.com",
- password: "123456",
- address_default: "89 rue Notre Dame, Bordeaux",
- latitude: 44.857123,
- longitude: -0.558934,
- role: "true"
-)
-
-user_pro7 = User.create!(
- first_name: "Emma",
- last_name: "Rousseau",
- email: "emma.rousseau@gmail.com",
- password: "123456",
- address_default: "12 place Gambetta, Bordeaux",
- latitude: 44.842789,
- longitude: -0.577456,
- role: "true"
-)
-
-user_pro7 = User.create!(
- first_name: "Isabella",
- last_name: "Martinez",
- email: "isabella.martinez@gmail.com",
- password: "123456",
- address_default: "23 avenue du Port, Le Teich",
- latitude: 44.6375,
- longitude: -1.0246,
- role: "true"
-)
-
-user_pro8 = User.create!(
- first_name: "Antoine",
- last_name: "Dubois",
- email: "antoine.dubois@gmail.com",
- password: "123456",
- address_default: "156 avenue de la Côte d'Argent, Le Teich",
- latitude: 44.6404,
- longitude: -1.0271,
- role: "true"
-)
-
-user_pro9 = User.create!(
- first_name: "Sophie",
- last_name: "Laurent",
- email: "sophie.laurent@gmail.com",
- password: "123456",
- address_default: "45 rue des Castaings, Biganos",
- latitude: 44.6528,
- longitude: -0.9687,
- role: "true"
-)
-
-  # Foodtruck 1
+# Existing food trucks
 foodtruck1 = Foodtruck.create!(
   name: "Sando Truck",
   phone_number: "07 66 28 54 36",
@@ -150,11 +154,38 @@ foodtruck1 = Foodtruck.create!(
   jours: "lundi, jeudi",
   user: user_pro1
 )
-file = URI.open("https://otuktuk.fr/wp-content/uploads/2017/10/o_tuk_tuk_fodd_truck_toulouse_slider.jpg")
+
+# Attacher une photo au Sando Truck
+file = URI.open(STOCK_PHOTOS.first)
 foodtruck1.photo.attach(io: file, filename: "sando_truck.jpg", content_type: "image/jpg")
 foodtruck1.save
 
-# Foodtruck 2
+# Créer 2 plats pour le Sando Truck
+2.times do |j|
+  dish = Dish.new(
+    foodtruck: foodtruck1,
+    title: DISH_TITLES.sample,
+    description: "Salade, tomate, oignons, sauce Biggy",
+    price: rand(8.50..18.50).round(2)
+  )
+
+  # Attacher une photo aléatoire de plat
+  file = URI.open(DISH_PHOTOS.sample)
+  dish.photo.attach(io: file, filename: "dish_sando_truck_#{j}.jpg", content_type: "image/jpg")
+  dish.save
+end
+
+# Créer des reviews pour le Sando Truck
+existing_review_users = [user1, user2]
+existing_review_users.each do |user|
+  Review.create(
+    foodtruck_id: foodtruck1.id,
+    user_id: user.id,
+    comment: ["Super food truck, je recommande!", "Une expérience culinaire délicieuse!", "Très bon rapport qualité-prix"].sample,
+    rating: rand(3..5)
+  )
+end
+
 foodtruck2 = Foodtruck.create!(
   name: "Le Van Foodtruck",
   phone_number: "07 69 63 28 30",
@@ -168,157 +199,117 @@ foodtruck2 = Foodtruck.create!(
   jours: "mardi, vendredi",
   user: user_pro2
 )
-file = URI.open("https://static.wixstatic.com/media/b45c09_d3b790add4cc4097afd338d22fe0f774~mv2.jpg/v1/fill/w_510,h_638,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/IMG_20240911_101704.jpg")
-foodtruck2.photo.attach(io: file, filename: "van_foodtruck.jpg", content_type: "image/jpg")
+file = URI.open(STOCK_PHOTOS.last)
+foodtruck2.photo.attach(io: file, filename: "le_van_truck.jpg", content_type: "image/jpg")
 foodtruck2.save
 
-# Foodtruck 3
-foodtruck3 = Foodtruck.create!(
-  name: "Truck de chef",
-  phone_number: "06 61 77 68 08",
-  categories: ["fr"],
-  description: "Une cuisine bistro de qualité, tout en fraîcheur, avec des recettes pratiques à manger.",
-  address_default: "Square Léonie Bonnet – Floirac",
-  latitude: 44.83238,
-  longitude: -0.514021,
-  status: true,
-  horaires: "12:00 à 15:00",
-  jours: "mercredi",
-  user: user_pro3
-)
-file = URI.open("https://otuktuk.fr/wp-content/uploads/2017/10/o_tuk_tuk_fodd_truck_toulouse_slider.jpg")
-foodtruck3.photo.attach(io: file, filename: "truck_chef.jpg", content_type: "image/jpg")
-foodtruck3.save
-
-# Foodtruck 4
-foodtruck4 = Foodtruck.create!(
-  name: "El taco del diablo",
-  phone_number: "09 80 80 04 92",
-  categories: ["mexicain", "californien"],
-  description: "La street food de Los Angeles revisitée à Bordeaux pour vous faire découvrir les tacos californiens.",
-  address_default: "87 Rue Lagrange 33000 BORDEAUX",
-  latitude: 44.8524179,
-  longitude: -0.581036,
-  status: true,
-  horaires: "19:00 à 22:30",
-  jours: "jeudi, samedi",
-  user: user_pro4
-)
-file = URI.open("https://static.wixstatic.com/media/369974_4b44793445374aee8e776c139cb8eedb~mv2.jpg/v1/crop/x_0,y_27,w_960,h_585/fill/w_434,h_270,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/369974_4b44793445374aee8e776c139cb8eedb~mv2.jpg")
-foodtruck4.photo.attach(io: file, filename: "taco_diablo.jpg", content_type: "image/jpg")
-foodtruck4.save
-
-# Foodtruck 5
-foodtruck5 = Foodtruck.create!(
-  name: "Yedi Tepe",
-  phone_number: "08 90 21 01 68",
-  categories: ["cuisine turque"],
-  description: "Embarquez pour un voyage de saveurs avec Yepi Tepe, votre food truck spécialisé dans la cuisine turque authentique !",
-  address_default: "6 Rue des Comètes - 33185 Le Haillan",
-  latitude: 44.8593025,
-  longitude: -0.669148,
-  status: false,
-  horaires: "12:00 à 15:00",
-  jours: "lundi, vendredi",
-  user: user_pro5
-)
-file = URI.open("https://les-seminaires.eu/wp-content/uploads/2023/07/food-truck-entreprise.jpg")
-foodtruck5.photo.attach(io: file, filename: "yedi_tepe.jpg", content_type: "image/jpg")
-foodtruck5.save
-
-# Foodtruck 6
-foodtruck6 = Foodtruck.create!(
-  name: "Seasons food truck",
-  phone_number: "06 31 86 58 03",
-  categories: ["cuisine de saison"],
-  description: "Respect des saisons et des produits locaux pour une cuisine fraîche et savoureuse.",
-  address_default: "place Paul Doumer (Chartrons)",
-  latitude: 44.8521802,
-  longitude: -0.5737263,
-  status: false,
-  horaires: "19:00 à 22:30",
-  jours: "mardi, dimanche",
-  user: user_pro6
-)
-file = URI.open("https://les-seminaires.eu/wp-content/uploads/2023/07/food-truck-entreprise.jpg")
-foodtruck6.photo.attach(io: file, filename: "seasons_foodtruck.jpg", content_type: "image/jpg")
-foodtruck6.save
-
-# Foodtruck 8
-foodtruck8 = Foodtruck.create!(
-  name: "Saveurs des Landes",
-  phone_number: "06 12 34 56 78",
-  categories: ["cuisine landaise"],
-  description: "Un voyage gustatif au cœur des Landes avec nos spécialités régionales revisitées.",
-  address_default: "156 avenue de la Côte d'Argent, Le Teich",
-  latitude: 44.6404,
-  longitude: -1.0271,
-  status: true,
-  horaires: "12:00 à 15:00",
-  jours: "mercredi, samedi",
-  user: user_pro8
-)
-file = URI.open("https://les-seminaires.eu/wp-content/uploads/2023/07/food-truck-entreprise.jpg")
-foodtruck8.photo.attach(io: file, filename: "saveurs_landes.jpg", content_type: "image/jpg")
-foodtruck8.save
-
-# Foodtruck 9
-foodtruck9 = Foodtruck.create!(
-  name: "Le Bistrot Roulant",
-  phone_number: "06 98 76 54 32",
-  categories: ["cuisine traditionnelle"],
-  description: "Cuisine traditionnelle française revisitée en street food avec des produits locaux.",
-  address_default: "45 rue des Castaings, Biganos",
-  latitude: 44.6528,
-  longitude: -0.9687,
-  status: false,
-  horaires: "19:00 à 22:30",
-  jours: "vendredi, dimanche",
-  user: user_pro9
-)
-file = URI.open("https://static.wixstatic.com/media/b45c09_d3b790add4cc4097afd338d22fe0f774~mv2.jpg/v1/fill/w_510,h_638,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/IMG_20240911_101704.jpg")
-foodtruck9.photo.attach(io: file, filename: "bistrot_roulant.jpg", content_type: "image/jpg")
-foodtruck9.save
-
-
-  # Dishes pour Sando Truck
-  dish1 = Dish.new(
-   foodtruck: foodtruck9,
-   title: "Veggie Sando",
-   description: "Pain de mie toasté, aubergine panée, chou blanc à la vinaigrette maison, sauce tonkatsu",
-   price: 12.50
+# Créer 2 plats pour Le Van Foodtruck
+2.times do |j|
+  dish = Dish.new(
+    foodtruck: foodtruck2,
+    title: DISH_TITLES.sample,
+    description: "Salade, tomate, oignons, sauce Biggy",
+    price: rand(8.50..18.50).round(2)
   )
-  file = URI.open("https://cdn.shopify.com/s/files/1/0605/9720/7189/files/20240821143137-img_1429.jpg?v=1724250702&width=1600&height=900")
-  dish1.photo.attach(io: file, filename: "veggie_sando.jpg", content_type: "image/jpg")
-  dish1.save
 
-  # dish2 = Dish.new(
-  #  foodtruck: foodtruck1,
-  #  title: "Porce Lait",
-  #  description: "Pain de mie toasté, porc IGP pané, chou blanc à la vinaigrette maison, sauce tonkatsu",
-  #  price: 14.50
-  # )
-  # file = URI.open("https://kissmychef.com/wp-content/uploads/2022/05/Sando.jpg")
-  # dish2.photo.attach(io: file, filename: "porce_lait.jpg", content_type: "image/jpg")
-  # dish2.save
+  # Attacher une photo aléatoire de plat
+  file = URI.open(DISH_PHOTOS.sample)
+  dish.photo.attach(io: file, filename: "dish_le_van_#{j}.jpg", content_type: "image/jpg")
+  dish.save
+end
 
-  # Dishes pour Le Van Foodtruck
-  dish3 = Dish.new(
-   foodtruck: foodtruck8,
-   title: "Galette de Sarrasin",
-   description: "à la fois tendre et délicatement parfumée, servie avec une garniture au choix : fromage, œuf, jambon ou légumes grillés.",
-   price: 10.90
+# Créer des reviews pour Le Van Foodtruck
+existing_review_users.each do |user|
+  Review.create(
+    foodtruck_id: foodtruck2.id,
+    user_id: user.id,
+    comment: ["Super food truck, je recommande!", "Une expérience culinaire délicieuse!", "Très bon rapport qualité-prix"].sample,
+    rating: rand(3..5)
   )
-  file = URI.open("https://www.cuisineactuelle.fr/imgre/fit/~1~cac~2023~01~17~999fccbd-d5ad-4cc2-91ba-d5a06206b882.jpeg/650x325/quality/80/crop-from/center/galette-de-sarrasin-a-l-ancienne.jpg")
-  dish3.photo.attach(io: file, filename: "galette_sarrasin.jpg", content_type: "image/jpg")
-  dish3.save
+end
 
-  dish4 = Dish.new(
-   foodtruck: foodtruck8,
-   title: "Crêpe sucrée",
-   description: "Une crêpe sucrée délicatement sucrée, que vous pourrez savourer nature ou accompagnée de garnitures gourmandes comme chocolat, caramel, fruits frais ou crème chantilly.",
-   price: 6.50
+# Génération des nouveaux food trucks
+55.times do |index|
+  # Définir la condition Le Teich avant son utilisation
+  le_teich_description = index <= 5 ?
+    "Food truck spécialisé dans la cuisine locale des environs du Bassin d'Arcachon." :
+    nil
+
+  # Sélection des coordonnées
+  if index <= 5
+    latitude, longitude = generate_coordinates_around_le_teich
+    city = "Le Teich et environs"
+  else
+    city, latitude, longitude = CITIES.sample
+  end
+
+  # Création de l'utilisateur professionnel
+  user_pro = User.create!(
+    first_name: FIRST_NAMES.sample,
+    last_name: LAST_NAMES.sample,
+    email: "pro_new_#{index}@example.com",
+    password: "123456",
+    address_default: "#{rand(1..200)} rue de #{city}",
+    latitude: latitude + rand(-0.1..0.1),
+    longitude: longitude + rand(-0.1..0.1),
+    role: "true"
   )
-  file = URI.open("https://www.ma-grande-taille.com/wp-content/uploads/2020/01/Une-idee-recettes-salees-sucrees-vegan-chandeleur-2020.jpg")
-  dish4.photo.attach(io: file, filename: "crepe_sucree.jpg", content_type: "image/jpg")
-  dish4.save
+
+  # Création du food truck
+  foodtruck = Foodtruck.create!(
+    name: FOOD_NAMES.sample,
+    phone_number: "0#{rand(6..7)}#{rand(10..99)} #{rand(10..99)} #{rand(10..99)} #{rand(10..99)}",
+    categories: FOOD_CATEGORIES.sample(rand(1..2)),
+    description: le_teich_description || "Un food truck unique proposant une cuisine #{FOOD_CATEGORIES.sample} de qualité dans les rues de #{city}.",
+    address_default: "#{rand(1..200)} rue de #{city}",
+    latitude: latitude,
+    longitude: longitude,
+    status: [true, false].sample,
+    horaires: rand(11..14).to_s + ":00 à " + rand(19..22).to_s + ":30",
+    jours: ["lundi, mercredi", "mardi, jeudi", "vendredi, samedi", "dimanche"].sample,
+    user: user_pro
+  )
+
+  # Attacher une photo aléatoire
+  file = URI.open(STOCK_PHOTOS.sample)
+  foodtruck.photo.attach(io: file, filename: "foodtruck_new_#{index}.jpg", content_type: "image/jpg")
+  foodtruck.save
+
+  # Même chose pour les plats
+  le_teich_dish_description = index <= 5 ?
+    "Huîtres chaudes, salade, tomate, oignons" :
+    nil
+
+  # Créer 2 plats pour chaque food truck
+  2.times do |j|
+    dish = Dish.new(
+      foodtruck: foodtruck,
+      title: DISH_TITLES.sample,
+      description: le_teich_dish_description || "Salade, tomate, oignons, sauce Biggy",
+      price: rand(8.50..18.50).round(2)
+    )
+
+    # Attacher une photo aléatoire de plat
+    file = URI.open(DISH_PHOTOS.sample)
+    dish.photo.attach(io: file, filename: "dish_new_#{index}_#{j}.jpg", content_type: "image/jpg")
+    dish.save
+  end
+
+  # Créer des reviews pour chaque food truck
+  le_teich_review_comment = index <= 5 ?
+    ["Découverte culinaire autour du Bassin d'Arcachon !", "Une adresse à ne pas manquer près du Teich"].sample :
+    nil
+
+  existing_review_users.each do |user|
+    review_comment = le_teich_review_comment.to_s if le_teich_review_comment.present?
+    review_comment ||= ["Super food truck, je recommande!", "Une expérience culinaire délicieuse!", "Très bon rapport qualité-prix"].sample
+    Review.create(
+      foodtruck_id: foodtruck.id,
+      user_id: user.id,
+      comment: review_comment,
+      rating: rand(3..5)
+    )
+  end
+end
+
+puts "Seed terminé avec succès !"
