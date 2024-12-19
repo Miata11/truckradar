e86d1fe8d647729b67cd6b8b66591fdf44b6e6b1
max poirier <maxpoirier030395@gmail.com>
2024-12-19 14:58:11 +0100

    maj

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :faq, :foodtrucks]
+  skip_before_action :authenticate_user!, only: [:home, :faq]
 
   def dashboard
     @favorites = current_user.favorites.includes(:foodtruck)
